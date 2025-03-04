c87c4612c308b8e74ed23ba99d7e11afefe4b93f
wfox <william.fox@corvidtec.com>
2025-03-04 16:32:06 -0500

    Updated PyPI version from last commit, which I forgot to do

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='zwoeafpy',
-    version='0.2.0',
+    version='0.3.0',
     author='William Fox',
     author_email='foxwilliamcarr@gmail.com',
     description='Python binding for the ZWO Electronic Automatic Focuser (EAF) SDK',
